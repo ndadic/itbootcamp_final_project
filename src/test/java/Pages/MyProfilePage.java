ca4d12ab3143f4c0949b029a493332e865a56380
ndadic <dadicnatalija@gmail.com>
2023-02-03 20:29:51 +0100

    Add MyProfilePage and ProfileTests

---
+++ src/test/java/Pages/MyProfilePage.java
@@ -0,0 +1,89 @@
+package Pages;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+//this.cityField.sendKeys(Keys.SPACE);
+//        this.cityField.sendKeys(Keys.CONTROL + "a");
+//        this.cityField.sendKeys(city);
+//        this.cityField.sendKeys(Keys.ARROW_DOWN);
+//        this.cityField.sendKeys(Keys.ENTER);
+public class MyProfilePage extends BasePage {
+    @FindBy(id = "phone")
+    private WebElement phoneField;
+
+    @FindBy(id = "city")
+    private WebElement cityField;
+
+    @FindBy(id = "country")
+    private WebElement countryField;
+
+    @FindBy(id = "urlTwitter")
+    private WebElement twitterField;
+
+    @FindBy(id = "urlGitHub")
+    private WebElement gitHubField;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[2]/span/form/div/div/div[8]/button")
+    private WebElement saveButton;
+
+    @FindBy (xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[4]/div/div/div/div/div[1]")
+    private WebElement saveMessage;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[4]/div/div/div[3]/button")
+    private WebElement close;
+
+    public MyProfilePage(WebDriver driver, WebDriverWait webDriverWait) {
+        super(driver, webDriverWait);
+    }
+
+    public void editProfile(String phone, String city, String country, String twitter, String gitHub) {
+        phoneField.clear();
+        phoneField.sendKeys(phone);
+        cityField.sendKeys(Keys.SPACE);
+        cityField.sendKeys(Keys.CONTROL + "a");
+        cityField.sendKeys(city);
+        cityField.sendKeys(Keys.ARROW_DOWN);
+        cityField.sendKeys(Keys.ENTER);
+        countryField.clear();
+        countryField.sendKeys(country);
+        twitterField.clear();
+        twitterField.sendKeys(twitter);
+        gitHubField.clear();
+        gitHubField.sendKeys(gitHub);
+        saveButton.click();
+
+    }
+
+    public WebElement getClose() {
+        return close;
+    }
+
+    public String saveMessage(){
+        String saveMessageText = saveMessage.getText();
+        return saveMessageText;
+    }
+
+    public WebElement getPhoneField() {
+        return phoneField;
+    }
+
+    public WebElement getCityField() {
+        return cityField;
+    }
+
+    public WebElement getCountryField() {
+        return countryField;
+    }
+
+    public WebElement getTwitterField() {
+        return twitterField;
+    }
+
+    public WebElement getGitHubField() {
+        return gitHubField;
+    }
+}
---
+++ src/test/java/Tests/ProfileTests.java
@@ -0,0 +1,35 @@
+package Tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ProfileTests extends BaseTest {
+
+    @BeforeMethod
+    @Override
+    public void beforeMethod() {
+        super.beforeMethod();
+        signupPage.openSignUpPage();
+        signupPage.signUp(faker.name().firstName(), faker.internet().emailAddress(), faker.internet().password());
+        phoneNumber = faker.phoneNumber().cellPhone();
+        country = faker.country().name();
+        twitterUrl = "https://" + faker.internet().url();
+        gitHubUrl = "https://" + faker.internet().url();
+    }
+
+    @Test
+    public void editProfile() {
+        homePage.clickOnMyProfile();
+        myProfilePage.editProfile(phoneNumber,"Chicago", country, twitterUrl, gitHubUrl);
+
+        Assert.assertTrue(myProfilePage.saveMessage().contains("Profile saved successfuly"));
+
+        Assert.assertEquals(myProfilePage.getPhoneField().getAttribute("value"), phoneNumber);
+        Assert.assertEquals(myProfilePage.getCityField().getAttribute("value"), "Chicago");
+        Assert.assertEquals(myProfilePage.getCountryField().getAttribute("value"), country);
+        Assert.assertEquals(myProfilePage.getTwitterField().getAttribute("value"), twitterUrl);
+        Assert.assertEquals(myProfilePage.getGitHubField().getAttribute("value"), gitHubUrl);
+    }
+}
